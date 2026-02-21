594ff774b813df262ee39aebba11f70e1c4d46eb
sstavh <sashatalabirshuk@gmail.com>
2026-02-21 18:48:19 +0200

    OurProcess develop V2

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import DirectorMes from '~/components/base/HiroPages/DirectorMes.vue';
 import Hero from '~/components/base/HiroPages/Hero.vue';
-
+import OurProcess from '~/components/base/HiroPages/OurProcess.vue';
 </script>
 
 <template>
     <section class="home">
+      
       <Hero /> 
+      <OurProcess /> 
       <DirectorMes />
-        
+      
     </section>
   </template>
   
