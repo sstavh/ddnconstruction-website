4bfe00bf31ec68270621f0fa95a7d609d54dc81d
sstavh <sashatalabirshuk@gmail.com>
2026-02-26 00:35:05 +0200

    General Hero section

---
+++ app/components/base/AboytPages/GeneralHero.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import Button from "../../ui/Button.vue";
+</script>
+
+<template>
+    <div class="container">
+        <div class="general-container">
+            <div class="general-container__box">
+                <div data-aos="fade-right" order: 2 class="foto-test">IMGS</div>
+                <div class="generel-box">
+                    <h1 class="generel-box__title">Текст слого для <br/>цієї сторінки</h1>
+                    <p class="generel-box__pidtext">текст для доповнненя</p>
+                    <Button order: 2 data-aos="fade-up"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.general-container{
+    margin-top: 160px;
+}
+
+.general-container__box{
+    display: flex;  
+    gap: 30px;
+    justify-content: center;
+}
+
+.foto-test{
+    width: 580px;
+    height: 500px;
+    background-color: blueviolet;
+}
+
+.generel-box{
+    margin-top: 90px;
+}
+
+.generel-box__pidtext{
+    padding:  20px 0px 20px 0px;
+    font-size: --font-s-textSmoll;
+    color: var(--color-praymeri-blekText);
+}
+</style>
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup>
 import HeroAboyt from '../components/base/AboytPages/HeroAboyt.vue';
+import GeneralHero from '../components/base/AboytPages/GeneralHero.vue';
+
 </script>
 <template>
     <section>
         <HeroAboyt/>
+        <GeneralHero/>
     </section>
 </template>
 <style scoped   ></style>
