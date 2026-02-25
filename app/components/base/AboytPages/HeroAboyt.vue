fe73c343ab7a76125ea6123a068ed4d7cbc68a7c
sstavh <sashatalabirshuk@gmail.com>
2026-02-26 00:12:00 +0200

    Hero Abaut Section

--- app/components/base/AboytPages/HeroAboyt.vue
+++ app/components/base/AboytPages/HeroAboyt.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-console.log("hero good woork");
 </script>
 
 <template>
@@ -7,7 +6,9 @@ console.log("hero good woork");
         <div class="container">
             <div class="heroAbaut-container">
                 <div class="heroAbout-container-box">
-                    <h1 class="heroAbout-box__title">Про  нас</h1>
+                    <h1 class="heroAbout-box__title">
+                        <span class="title-text">Про нас</span>
+                    </h1>
                 </div>
             </div>
         </div>
@@ -19,13 +20,36 @@ console.log("hero good woork");
     height: 400px;
     background: linear-gradient(
         to top,
-        rgba(0, 0, 0.4, 0.8),   /* темний низ */
-        rgba(0, 0, 0, 0)      /* прозорий верх */
+        rgba(0, 0, 0, 0.8),
+        rgba(0, 0, 0, 0)
     ),
-    blueviolet; /* основний колір */
+    blueviolet;
 }
 
 .heroAbout-box__title{
+    padding-top: 11%;
+    text-align: center;
+    width: 100%;
+    display: inline-block;
+    border-bottom: 2px solid var(--color-secondary-steel);
+    padding-bottom: 40px;
+}
 
+/* Анімується тільки текст */
+.title-text{
+    display: inline-block;
+    opacity: 0;
+    animation: fadeUp 1s ease forwards;
+}
+
+@keyframes fadeUp{
+    from{
+        opacity: 0;
+        transform: translateY(40px);
+    }
+    to{
+        opacity: 1;
+        transform: translateY(0);
+    }
 }
 </style>
