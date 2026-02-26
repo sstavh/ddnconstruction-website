4dc98de1e006157d2f6676d8548f9d8636a6dbfd
sstavh <sashatalabirshuk@gmail.com>
2026-02-26 02:05:45 +0200

    Difficulties section

---
+++ app/components/base/AboytPages/Difficulties.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup></script>
+
+<template>
+    <section>
+        <div class="container">
+            <div class="diffic-container">
+                <div class="diffic-container__box">
+                    <h3 data-aos="fade-up" class="diffic-box__tile">Трудності в роботі</h3>
+                    <p class="diffic-box__pidtext">текст досить опширний і з хороши змістом і роботою агігаішгапінанімаіпам іп гапінап іап інамііма вшф8нвф98 вфва фвфв вф8 вшрвгфрв фрв ф8вн фвнрв гшфрвгшвфшгвфгв фав фнвп гірв ф78вф98в ф98вфврфв мимфрпвф </p>
+                </div> 
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.diffic-container{
+    margin-top: 160px;
+}
+
+.diffic-box__tile{
+    text-align: center;
+    margin-bottom: 50px;
+}
+
+.diffic-box__pidtext{
+    padding-left: 5%;
+    padding-right: 5%;
+    text-align: center;
+}
+</style>
+
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -3,6 +3,7 @@ import HeroAboyt from '../components/base/AboytPages/HeroAboyt.vue';
 import GeneralHero from '../components/base/AboytPages/GeneralHero.vue';
 import StatsCounters from '../components/base/AboytPages/StatsCounters.vue';
 import Historywork from '../components/base/AboytPages/Historywork.vue';
+import Difficulties from '../components/base/AboytPages/Difficulties.vue';
 
 </script>
 <template>
@@ -11,6 +12,7 @@ import Historywork from '../components/base/AboytPages/Historywork.vue';
         <GeneralHero/>
         <StatsCounters/>    
         <Historywork/>
+        <Difficulties/>
     </section>
 </template>
 <style scoped></style>
