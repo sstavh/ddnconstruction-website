49da9b8c19f7f6f41bb9dbfadfa2ffed3cdbea79
sstavh <sashatalabirshuk@gmail.com>
2026-03-13 18:21:59 +0200

    Collected V1 and text blok updeta and Promo Card

---
+++ app/components/base/ServisesPages/СollectedServises.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import PromoCard from '../../ui/ServisesComponents/PromoCard.vue'
+import BeforeAfter from '../../ui/informationBlok/beforeAfter.vue';
+import TextBlok from '../../ui/informationBlok/TextBlok.vue';
+
+</script>
+
+<template>
+    <section>
+        <div class="container">
+            <div class="collected-contaoner">
+                <h3 class="collected-title">
+                    Вибирай що тобі потрібно
+                </h3>
+
+                <div class="collected-box">
+                     <PromoCard
+  logo="/logos/spotify.svg"
+  color="#1db954"
+  width="380px"
+  height="450px"
+  bg-image="/images/music.jpg"
+  link="/music"
+  button-text="Перейти"
+/>
+<BeforeAfter/>
+<PromoCard
+  logo="/logos/spotify.svg"
+  color="#1db954"
+  width="380px"
+  height="450px"
+  bg-image="/images/music.jpg"
+  link="/music"
+  button-text="Перейти"
+/>
+<PromoCard
+  logo="/logos/spotify.svg"
+  color="#1db954"
+  width="380px"
+  height="450px"
+  bg-image="/images/music.jpg"
+  link="/music"
+  button-text="Перейти"
+/>
+<PromoCard
+  logo="/logos/spotify.svg"
+  color="#1db954"
+  width="380px"
+  height="450px"
+  bg-image="/images/music.jpg"
+  link="/music"
+  button-text="Перейти"
+/>
+<TextBlok
+    text="Це текст із головного файлу. Тут ти можеш писати будь-який опис для інформаційного блока."
+  />
+  <PromoCard
+  logo="/logos/spotify.svg"
+  color="#1db954"
+  width="380px"
+  height="450px"
+  bg-image="/images/music.jpg"
+  link="/music"
+  button-text="Перейти"
+/>
+<PromoCard
+  logo="/logos/spotify.svg"
+  color="#1db954"
+  width="380px"
+  height="450px"
+  bg-image="/images/music.jpg"
+  link="/music"
+  button-text="Перейти"
+/>
+
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped></style>
--- app/pages/service.vue
+++ app/pages/service.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import HeroServises from '../components/base/ServisesPages/HeroServises.vue';
-
+import CollectedServises from '../components/base/ServisesPages/СollectedServises.vue'
 </script>
 <template>
     <HeroServises/>
+    <CollectedServises/>
 </template>
 <style scoped></style>
