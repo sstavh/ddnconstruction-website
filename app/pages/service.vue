6b5e15c6bf477e8598d432a035c392be68ab859a
sstavh <sashatalabirshuk@gmail.com>
2026-03-13 17:36:26 +0200

    Hero section

---
+++ app/pages/service.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HeroServises from '../components/base/ServisesPages/HeroServises.vue';
+
+</script>
+<template>
+    <HeroServises/>
+</template>
+<style scoped></style>
